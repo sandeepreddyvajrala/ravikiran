5c061dd802974ba7d8ba2c89d0724cca131408c5
fis <fis@fiss-mac.local>
2016-10-26 05:15:53 -0700

    Initial Commit

---
+++ hello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hello
+//
+//  Created by fis on 10/26/16.
+//  Copyright © 2016 fis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hello
+//
+//  Created by fis on 10/26/16.
+//  Copyright © 2016 fis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
